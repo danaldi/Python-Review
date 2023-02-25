5683799388c930323911e3d479f6a6148c6a1aab
danaldi <18120070@std.stei.itb.ac.id>
2023-02-25 16:45:08 +0700

    python review

---
+++ main.py
@@ -0,0 +1,18 @@
+# Data types
+# 1. int -> doesnt have any decimal point
+# 2. float -> has decimal point
+# 3. str-> anything in quotes
+# 4. bool-> true or false
+
+# Output and printing
+print("Hello World") # Hello World
+print(2+3) # 5
+print(2.5+3.5) # 6.0
+print("2"+"3") # 23
+print('hello','end',87,False) # hello end 87 False
+
+# Variables
+hello = "Hello"
+saddan = "Saddan"
+
+print(hello,saddan) # Hello Saddan
